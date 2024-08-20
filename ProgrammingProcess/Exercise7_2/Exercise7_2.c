1037349f82acf328867b6f142e03ab45274cc65e
Luis Alvarez <alvarez.en.luis@gmail.com>
2024-08-20 15:18:00 -0700

    working on exercise 7-2

---
+++ ProgrammingProcess/Exercise7_2/Exercise7_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+char line[100];
+
+int main() {
+
+  Date
+
+  while(1){
+
+    printf("Enter an imperial value along with its unit: ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%lf %s", &value, unit);
+
+  }
+
+}
