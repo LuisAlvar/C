ece2560993b66dddfcad912ea5ff3a8caea38643
luis <alvarez.en.luis@gmail.com>
2024-08-20 20:11:13 -0700

    working on ex: 7-2

--- ProgrammingProcess/Exercise7_2/Exercise7_2.c
+++ ProgrammingProcess/Exercise7_2/Exercise7_2.c
@@ -1,19 +1,96 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
 #include <string.h>
+#include <stdbool.h>
+
+#define DATE_CHAR_ARRAY_SIZE 10
 
 char line[100];
 
+struct DateTime {
+  time_t time;
+  struct tm st_time;
+  int year;
+  int day;
+  int month;
+};
+
+
+int process_dates(char date1[DATE_CHAR_ARRAY_SIZE], char date2[DATE_CHAR_ARRAY_SIZE]);
+void sub_str(char *source, char *dest, int start_index, int size);
+struct DateTime convert_dates_to_int_data(char date[DATE_CHAR_ARRAY_SIZE]);
+
+
 int main() {
 
-  Date
+  char date_1[DATE_CHAR_ARRAY_SIZE];
+  char date_2[DATE_CHAR_ARRAY_SIZE];
+  int days = 0;
 
   while(1){
 
-    printf("Enter an imperial value along with its unit: ");
+    printf("Enter first date (mm/dd/yyyy): ");
     fgets(line, sizeof(line), stdin);
-    sscanf(line, "%lf %s", &value, unit);
+    sscanf(line, "%s", date_1);
+    if(!strcmp(date_1,"q")) break;
 
+    printf("Enter second date (mm/dd/yyyy): ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%s", date_2);
+    if(!strcmp(date_2,"q")) break;
+
+    days = process_dates(date_1, date_2);
+    printf("Number of days between the dates: %d\n", days);
   }
 
+  return 0;
+}
+
+
+
+int process_dates(char date1[DATE_CHAR_ARRAY_SIZE], char date2[DATE_CHAR_ARRAY_SIZE]) {
+
+  struct DateTime dt1;
+  dt1 = convert_dates_to_int_data(date1);
+
+
+  
+  return 0;
+}
+
+
+struct DateTime convert_dates_to_int_data(char date[DATE_CHAR_ARRAY_SIZE]) {
+
+  struct DateTime dt;
+  char *month = malloc(2 * sizeof(char));
+  char *day = malloc(2 * sizeof(char));
+  char *year = malloc(4 * sizeof(char));
+  
+  sub_str(date, month, 0, 2);
+  sub_str(date, day, 3, 2);
+  sub_str(date, year, 6, 4);
+
+  dt.month = atoi(month);
+  dt.day = atoi(day);
+  dt.year = atoi(year);
+
+  dt.st_time.tm_year = dt.year - 1900;
+  dt.st_time.tm_mon = dt.month - 1;
+  dt.st_time.tm_mday = 
+
+  free(month);
+  free(day);
+  free(year);
+
+  return dt;
+}
+
+
+void sub_str(char *source, char *dest, int start_index, int size){
+  int index = start_index;
+  while (index < start_index + size) {
+    dest[strlen(dest)] = source[index];
+    ++index;
+  }
 }
--- ProgrammingProcess/Exercise7_2/README.md
+++ ProgrammingProcess/Exercise7_2/README.md
@@ -1,36 +1,28 @@
-# Convert: Conversion of Imperial To Metric Units
+# DaysInBetween: Calculate the Day Difference Between Two Dates
 ## Preliminary Specification 
 >Warning: This document is a preliminary specification. Any resemblance to any software living or dead is purely coincidental. 
 
-Convert is a program that allows the user enter any imperial unit value and be confidental that the metric unit value conversion is precise and accurry. 
+DaysInBetween is a program that allows the user enter two different days: first date before the second date. It will return the days difference between these two dates. 
 
-When the program runs, it will display a prompt to enter an imperial value along with its unit. The result will be a metric value along with a unit.
+When the program runs, it will display a prompt to enter an first date, and then re-prompted to enter a second date. Afterward, it will display the the days between the two dates. 
 
-The initial verison of this program will contain 
-a short list of unit conversion requested by client. 
-
-### Length 
-|Imperial Unit| Metric Units|
-|:------------|-------------|
-|Inches (in)  |Meter (m)|
-|Feet (ft)    |Meter (m)|
-|Yard (yd)    |Meter (m)|
-|Mile         |Meter (m)|
 
 For example: (user input is in boldface)
-> Enter a imperial value along with unit: <strong>12 in</strong>
-<br/>
-> Result: 30.48 cm
-
+```text
+Enter first date (mm/dd/yyy): 06/06/1990
+Enter second date (mm/dd/yyy): 04/03/1992
+Number of days in between: ### day(s)
+```
 
 ## Code Design 
 Our conversion app will not use any files or require any type of data structures. What's left for this phase is to design the major algorithm.
 
-```bash
+```text
 Loop
-  Read value and imperial unit. 
-  Map imperial unit to metric units
-  Compute the metric conversion 
+  Read first date
+  Read second date
+  Map date to time_t object
+  Compute the number of days in between
   Display the result
 End-Loop
 ```
