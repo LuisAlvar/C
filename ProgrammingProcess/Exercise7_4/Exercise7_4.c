574328435eae55eed31036b271e16a4c1006cbd8
Luis Alvarez <alvarez.en.luis@gmail.com>
2024-08-22 17:34:52 -0700

    working on Exercise 7_4

--- ProgrammingProcess/Exercise7_4/Exercise7_4.c
+++ ProgrammingProcess/Exercise7_4/Exercise7_4.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_SERIAL_TRANSMISSION_SIZE_BUFFER 960
-#define SECONDS_IN_A_MINUTE 60
-#define MINUTES_IN_A_HOUR 60
-#define HOURS_IN_A_DAY 24
+#define CA_SALES_TAX .08
 
 char line[100];
 
+
 int main() {
 
-  char send_buffer[MAX_SERIAL_TRANSMISSION_SIZE_BUFFER];
-  int filesize_in_bytes;
-  double completion_eta;
+  double sub_total = 0.0;
+  double total = 0;
 
-  printf("Enter file size: ");
+  printf("Enter a sub_total: ");
   fgets(line, sizeof(line), stdin);
-  sscanf(line, "%d", &filesize_in_bytes);
+  sscanf(line, "%f", &sub_total);
 
-  int max_send_buffer_size_bytes_per_second =  sizeof(send_buffer);
-  completion_eta = (filesize_in_bytes/max_send_buffer_size_bytes_per_second) * (1.0/SECONDS_IN_A_MINUTE) * (1.0/MINUTES_IN_A_HOUR) * (1.0/HOURS_IN_A_DAY);
+  total =  sub_total + (sub_total * CA_SALES_TAX);
 
-  printf("Based on the limitation of our hardware, max transmission of 960 bytes per second is allowed.\n");
-  printf("A file size of %d bytes will take %.2f days.\n", filesize_in_bytes, completion_eta);
+  printf("Your total is %.2f", total);
 
   return 0;
 }
