a93876e4204c2bef8d399092eb3131a6097b8329
luis <alvarez.en.luis@gmail.com>
2024-08-23 20:08:17 -0700

    still working on it

---
+++ MoreControlStatements/Exercise8_4/Exercise8_4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+char line[100];
+double number = 0;
+double total = 0.0;
+double sum = 0.0;
+int counter = 0;
+
+int main() {
+
+  return 0;
+}
